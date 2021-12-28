eed06abf17b22663b6390a32f66ca581de152202
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-12-28 17:25:26 -0500

    Create leetcode_paint_house.cpp

---
+++ leetcode_paint_house.cpp
@@ -0,0 +1,42 @@
+/*
+Dynamic programming using bottom-up method
+* The house-painting problem
+You want to paint 3 houses with color (Red or Blur or Green) at a minimal total cost , 
+the consecutive houses cannot have the same color.
+
+* 2-D State definition:
+state (i, j): the minimal cost for painting i houses and the i-th house was painted with color j.
+* State transition:
+state (m1, n1) can transit to state (m2, n2) by painting a house with a color.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num_houses = 3, num_colors = 3;
+    vector <vector <int> > costs = {{17, 2, 17}, {16, 16, 5}, {14, 3, 19}};
+    vector <vector<int> > memo(num_houses+1, vector<int>(num_colors, INT_MAX));
+    // the 1st house
+    for (int j=0; j<num_colors; j++)
+    {
+        memo[1][j] = costs[0][j];
+    }
+    
+    for (int i = 2; i <= num_houses; i++)
+    {
+        for(int j=0; j<num_colors; j++)
+        {
+            for (int k=0; k<num_colors; k++)
+            {
+                if (k!=j && memo[i-1][k] + costs[i-1][j] < memo[i][j]) 
+                {
+                    memo[i][j] = memo[i-1][k] + costs[i-1][j];
+                }
+            }
+        }
+    }
+    
+    cout << *min_element(memo[num_houses].begin(), memo[num_houses].end()) << endl;
+}
