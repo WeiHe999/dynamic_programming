3fa5c3ad8620b1ca8b2cf9b546fe06c0b27f601b
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-12-30 11:24:02 -0500

    Create bottom_up_max_product_subarray.cpp

---
+++ bottom_up_max_product_subarray.cpp
@@ -0,0 +1,19 @@
+/*
+Dynamic programming using bottom-up method
+* The Maximum Product Subarray problem
+Find the max product of the consecutive sub-array from an interger array [2, 3, -2, 4]
+
+* 1-D State definition:
+state-i: the max product of the consecutive sub-array ending at the i-th element
+
+* State transition:
+State-m can transit to state-(m+1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+}
