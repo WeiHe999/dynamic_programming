ba9daff4bd266fec28281e48dcaca532a07bd574
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-12-30 20:38:32 -0500

    Update bottom_up_max_product_subarray.cpp

--- bottom_up_max_product_subarray.cpp
+++ bottom_up_max_product_subarray.cpp
@@ -16,23 +16,15 @@ using namespace std;
 int main()
 {
     vector <int> vec1 = {2, -3, -2, 4, -5, -5, -5};
-    vector <pair<int, int> > memo;
-    for (auto x: vec1)
-    {
-       memo.push_back({x, x}); //{min, max} 
-    }
+    vector <pair <int, int> > memo;
+    for (auto x : vec1) memo.push_back({x, x});
     for (int i = 1; i < vec1.size(); i++)
     {
-        // min
-        int min_value = min( min(memo[i - 1].first * vec1[i], memo[i - 1].second * vec1[i]), memo[i].first);
-        // max
-        int max_value = max( max(memo[i - 1].first * vec1[i], memo[i - 1].second * vec1[i]), memo[i].second);
-        memo[i] = {min_value, max_value};
+        int a = min(min(memo[i - 1].first * vec1[i], memo[i - 1].second * vec1[i]), memo[i].first);
+        int b = max(max(memo[i - 1].first * vec1[i], memo[i - 1].second * vec1[i]), memo[i].second);
+        memo[i] = {a, b};
     }
-    int final_max = INT_MIN;
-    for(auto x : memo)
-    {
-        if (x.second > final_max) final_max = x.second;
-    }
-    cout << final_max << endl;
+    int max = INT_MIN;
+    for (auto a : memo) if (a.second > max) max = a.second;
+    cout << max << endl;
 }
