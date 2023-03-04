6fbcf87d01d7634d7943ca727e4e144ad830309d
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2023-03-04 07:44:29 -0500

    Update 0_0dp_templates.cpp

--- 0_0dp_templates.cpp
+++ 0_0dp_templates.cpp
@@ -12,6 +12,7 @@ items are put into a vector as below:
     {item_2_level_1_weight, item_2_level_1_value, item_2_level_1_categoty}} 
     category: 0 is 0/1 knapsack, 1 is unlimited pnapsack
  */
+
 int conditional_knapsack(vector<vector<int> > items, int num_items, int num_levels, int weight_capacity)
 {
     /*
@@ -46,28 +47,26 @@ int conditional_knapsack(vector<vector<int> > items, int num_items, int num_leve
             else
             {
                 // unlimited knapsack, forward update
+                memo[level_id+1] = memo[level_id]; //copy to the next level
                 for (int k = 0; k <= weight_capacity; k++)
                 {
-                    if (memo[level_id][k]==-1) continue;
+                    if (memo[level_id+1][k]==-1) continue;
                     if (k + weight <= weight_capacity) 
                     {
-                        memo[level_id + 1][k + weight] = max(memo[level_id + 1][k + weight], memo[level_id][k] + value);
-                        vec_max[k + weight] = max(vec_max[k + weight], memo[level_id + 1][k + weight]);
+                        memo[level_id +1 ][k + weight] = max(memo[level_id+1][k + weight], memo[level_id+1][k] + value);
+                        vec_max[k + weight] = max(vec_max[k + weight], memo[level_id+1][k + weight]);
                         //cout << "update (" << level_id+1 << ", " << k+weight << ") to " << memo[level_id+1][k+weight] << endl;
                     }
                 }                  
             }
-
         }
         // print(vec_max);
         // cout << "***" << endl;
         // for (int m=0; m<=num_levels; m++) print(memo[m]);
         // cout << endl;
-        
         vector<vector<int> > tmp(num_levels + 1, vector<int>(weight_capacity + 1, -1));
         tmp[0] = vec_max;
         memo = tmp;
-
     }
     int max_value = -1;
     for (int k=0; k <= weight_capacity; k++)
